80cc6511174f5fe9700eb8516359143f60477b40
Abhishek Saxena <abhishek.saxena@appdynamics.com>
2021-12-09 12:09:28 +0530

    Adding files from old repo

---
+++ CHANGELOG.md
@@ -0,0 +1,23 @@
+
+## 2.0.0 - April 17, 2018
+
+1. Moved extension to AppD AWS 2.0.0 commons framework
+2. Added rate limit support, Refer https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cloudwatch_limits.html
+3. Added dynamic metric collection in sync with CloudWatch monitoring level, Refer https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/using-cloudwatch-new.html
+4. Added include metrics and removed exclude metrics.
+5. Added time range configuration for individual metric
+
+## 2.0.1 - 22nd March, 2019
+1. Updated the aws-cloudwatch-exts-commons to 2.0.1.1 from 2.0.0
+
+## 2.1.2 - 30th April, 2020
+1. Updated the aws-cloudwatch-exts-commons to 2.2.2
+
+## 2.1.3 - 7th May, 2020
+1. Updated the aws-cloudwatch-exts-commons to 2.2.3.1
+
+## 2.1.4 - 19th Jan, 2021
+1. Updated the aws-cloudwatch-exts-commons to 2.2.4
+
+## 2.1.5 - 14th Apr, 2021
+1. Updated the aws-cloudwatch-exts-commons to 2.2.5
---
+++ src/main/java/com/appdynamics/extensions/aws/ec2/EC2Monitor.java
@@ -0,0 +1,98 @@
+/*
+ *   Copyright 2018. AppDynamics LLC and its affiliates.
+ *   All Rights Reserved.
+ *   This is unpublished proprietary source code of AppDynamics LLC and its affiliates.
+ *   The copyright notice above does not evidence any actual or intended publication of such source code.
+ *
+ */
+
+package com.appdynamics.extensions.aws.ec2;
+
+import static com.appdynamics.extensions.aws.Constants.METRIC_PATH_SEPARATOR;
+
+import com.appdynamics.extensions.aws.SingleNamespaceCloudwatchMonitor;
+import com.appdynamics.extensions.aws.collectors.NamespaceMetricStatisticsCollector;
+import com.appdynamics.extensions.aws.ec2.config.EC2Configuration;
+import com.appdynamics.extensions.aws.ec2.providers.EC2InstanceNameProvider;
+import com.appdynamics.extensions.aws.metric.processors.MetricsProcessor;
+import com.appdynamics.extensions.logging.ExtensionsLoggerFactory;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Florencio Sarmiento
+ */
+public class EC2Monitor extends SingleNamespaceCloudwatchMonitor<EC2Configuration> {
+
+    private static final Logger LOGGER = ExtensionsLoggerFactory.getLogger(EC2Monitor.class);
+
+    private static final String DEFAULT_METRIC_PREFIX = String.format("%s%s%s%s",
+            "Custom Metrics", METRIC_PATH_SEPARATOR, "Amazon EC2", METRIC_PATH_SEPARATOR);
+
+    public EC2Monitor() {
+        super(EC2Configuration.class);
+        LOGGER.info(String.format("Using AWS EC2 Monitor Version [%s]",
+                this.getClass().getPackage().getImplementationTitle()));
+    }
+
+    protected void initialiseServiceProviders(EC2Configuration config) {
+
+        EC2InstanceNameProvider ec2InstanceNameProvider = EC2InstanceNameProvider.getInstance();
+        ec2InstanceNameProvider.initialise(config.getAccounts(),
+                config.getCredentialsDecryptionConfig(),
+                config.getProxyConfig(), config.getEc2InstanceNameConfig(), config.getTags(),
+                config.getMetricsConfig().getMaxErrorRetrySize());
+    }
+
+    @Override
+    public String getDefaultMetricPrefix() {
+        return DEFAULT_METRIC_PREFIX;
+    }
+
+    @Override
+    public String getMonitorName() {
+        return "EC2Monitor";
+    }
+
+    @Override
+    protected List<Map<String, ?>> getServers() {
+        return new ArrayList<Map<String, ?>>();
+    }
+
+    @Override
+    protected void initialize(EC2Configuration config) {
+        super.initialize(config);
+        initialiseServiceProviders(config);
+    }
+
+    @Override
+    protected NamespaceMetricStatisticsCollector getNamespaceMetricsCollector(
+            EC2Configuration config) {
+        MetricsProcessor metricsProcessor = createMetricsProcessor(config);
+
+        return new NamespaceMetricStatisticsCollector
+                .Builder(config.getAccounts(),
+                config.getConcurrencyConfig(),
+                config.getMetricsConfig(),
+                metricsProcessor,
+                config.getMetricPrefix())
+                .withCredentialsDecryptionConfig(config.getCredentialsDecryptionConfig())
+                .withProxyConfig(config.getProxyConfig())
+                .build();
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return LOGGER;
+    }
+
+    private MetricsProcessor createMetricsProcessor(EC2Configuration config) {
+        return new EC2MetricsProcessor(
+                config.getMetricsConfig().getIncludeMetrics(),
+                config.getEc2Instance());
+    }
+}
